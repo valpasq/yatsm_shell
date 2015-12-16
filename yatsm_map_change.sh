d5372fd168d514a3f4c5f3d465a17ee110094377
Valerie <valpasq@bu.edu>
2015-12-15 19:39:57 -0500

    generalized script to handle data from different runs

--- yatsm_map_change.sh
+++ yatsm_map_change.sh
@@ -4,8 +4,6 @@
 #$ -N map_change
 #$ -j y
 
-source ~/conda/bin/activate yatsm
-
 if [ -z "$1" ]; then
     echo "Error - please specify a directory with extraced Landsat archives. Usage:"
     echo "    $0 <directory>"
@@ -16,13 +14,14 @@ fi
 here=$1
 cd $here
 
+run=$2
+
 startdate=1982-01-01
 enddate=2015-12-31
 
 # Generate change map
-yatsm -v changemap first --magnitude $startdate $enddate ./changemap_first.gtif
+yatsm -v changemap --result ./${run}_YATSM/ first $startdate $enddate ./${run}_changemap_first.gtif
 
-yatsm -v changemap last --magnitude $startdate $enddate ./changemap_last.gtif
+yatsm -v changemap --result ./${run}_YATSM/ last $startdate $enddate ./${run}_changemap_last.gtif
 
-yatsm -v changemap num $startdate $enddate ./changemap_num.gtif
-
+yatsm -v changemap --result ./${run}_YATSM/ num $startdate $enddate ./${run}_changemap_num.gtif
