f6677fd2775486279847758925f3df392194525b
Valerie <valpasq@bu.edu>
2016-01-30 18:28:09 -0500

    removed load environment command, added general run input

--- yatsm_map_amplitude.sh
+++ yatsm_map_amplitude.sh
@@ -4,8 +4,6 @@
 #$ -N map_amplitudes
 #$ -j y
 
-source ~/conda/bin/activate yatsm
-
 if [ -z "$1" ]; then
     echo "Error - please specify a directory with extraced Landsat archives. Usage:"
     echo "    $0 <directory>"
@@ -16,23 +14,30 @@ fi
 here=$1
 cd $here
 
-# Second input is date for harmonic extraction, e.g. 2006-06-01
-date=$2
+# Second input is run name
+run=$2
+
+# Third input is date for harmonic extraction, e.g. 2006-06-01
+date=$3
 
 # Band 1
 yatsm -v map --band 1 \
+--result ./YATSM_${run} \
 --ndv -9999 --before --amplitude coef $date ./harm_b1.gtif
 
 # Band 2
 yatsm -v map --band 2 \
+--result ./YATSM_${run} \
 --ndv -9999 --before --amplitude coef $date ./harm_b2.gtif
 
 # Band 3
 yatsm -v map --band 3 \
+--result ./YATSM_${run} \
 --ndv -9999 --before --amplitude coef $date ./harm_b3.gtif
 
 # Band 4
 yatsm -v map --band 4 \
+--result ./YATSM_${run} \
 --ndv -9999 --before --amplitude coef $date ./harm_b4.gtif
 
 # Band 5
@@ -41,20 +46,25 @@ yatsm -v map --band 5 \
 
 # Band 6
 yatsm -v map --band 6 \
+--result ./YATSM_${run} \
 --ndv -9999 --before --amplitude coef $date ./harm_b6.gtif
 
 # Band 7
 yatsm -v map --band 7 \
+--result ./YATSM_${run} \
 --ndv -9999 --before --amplitude coef $date ./harm_b7.gtif
 
 # Band 8
 yatsm -v map --band 8 \
+--result ./YATSM_${run} \
 --ndv -9999 --before --amplitude coef $date ./harm_B.gtif
 
 # Band 9
 yatsm -v map --band 9 \
+--result ./YATSM_${run} \
 --ndv -9999 --before --amplitude coef $date ./harm_G.gtif
 
 # Band 10
 yatsm -v map --band 10 \
+--result ./YATSM_${run} \
 --ndv -9999 --before --amplitude coef $date ./harm_W.gtif
